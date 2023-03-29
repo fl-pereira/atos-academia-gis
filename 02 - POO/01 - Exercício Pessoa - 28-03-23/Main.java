365008f66239533c7563532885e05b4a05160e2c
Felipe Pereira <62648916+fl-pereira@users.noreply.github.com>
2023-03-29 19:27:41 -0300

    sync

--- 02 - POO/01 - Exercício Pessoa - 28-03-23/Main.java
+++ 02 - POO/01 - Exercício Pessoa - 28-03-23/Main.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
+        // Cria um objeto para receber dados do usuário
         Scanner sc = new Scanner(System.in);
 
+        // Imprime a mensagem no console
         System.out.println("Insira seu nome: ");
+        // Atribui o dado inserido pelo usuário para a variável "nome"
         String nome = sc.next();
         System.out.println("Insira sua idade: ");
+        // Atribui o dado inserido pelo usuário para a variável "idade"
         Integer idade = sc.nextInt();
 
+        // Cria uma instância da classe-objeto Pessoa com o nome "p" e atribui as variáveis "nome" e "idade"
         Pessoa p = new Pessoa(nome, idade);
 
-        System.out.printf("### BEM-VINDO, %s ###%n", p.getNome());
-        if(p.getIdade() == 0 || p.getIdade() < 0){
-            System.out.println("Ops, acho que a idade não pode ser zero ou menor que zero.");
-        } else if (p.getIdade() > 0 && p.getIdade() <= 20) {
-            System.out.println("Você é jovem.");
-        } else if (p.getIdade() > 20 && p.getIdade() <= 60) {
-            System.out.println("Agora você é adulto. Pague os boletos.");
-        } else System.out.println("Você é um idoso, hora de descansar e curtir a vida.");
+        // O objeto "p" da classe-objeto Pessoa tem acesso aos seus métodos
+        p.saudacao(nome);
+        p.verificaIdade(idade);
 
         sc.close();
         
--- 02 - POO/01 - Exercício Pessoa - 28-03-23/Pessoa.java
+++ 02 - POO/01 - Exercício Pessoa - 28-03-23/Pessoa.java
@@ -1,15 +1,19 @@
 public class Pessoa {
 
+    // Cria os atributos da classe e os encapsula para protegê-los
     private String nome;
     private Integer idade;
 
+    // Cria um construtor padrão, sem assinatura
     public Pessoa(){}
 
+    // Cria um construtor com a assinatura, obrigando a inserir nome e idade
     public Pessoa(String nome, Integer idade){
         this.nome = nome;
         this.idade = idade;
     }
 
+    // Cria os getters e setters
     public String getNome() {
         return nome;
     }
@@ -24,6 +28,22 @@ public class Pessoa {
 
     public void setIdade(Integer idade) {
         this.idade = idade;
+    }
+
+    // Cria um método void para saudar o usuário
+    public void saudacao(String nome){
+        System.out.println("### Bem-vindo, " + nome + " ###");
+    }
+
+    // Cria um método que envia uma mensagem de acordo com a idade inserida
+    public void verificaIdade(Integer idade){
+        if(idade == 0 || idade < 0){
+            System.out.println("Ops, acho que a idade não pode ser zero ou menor que zero.");
+        } else if (idade > 0 && idade <= 20) {
+            System.out.println("Você é jovem.");
+        } else if (idade > 20 && idade <= 60) {
+            System.out.println("Agora você é adulto. Pague os boletos.");
+        } else System.out.println("Você é um idoso, hora de descansar e curtir a vida.");
     }
        
 }
